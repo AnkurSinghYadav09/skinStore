2509f17b88b5cc09bda1653ed87f1193e76c86a0
AnkurSinghYadav09 <ankursingh989@gmail.com>
2022-06-14 17:19:18 +0530

    update

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
 const Navbar = () => {
   return (
-    <div className="nav" >
-        <div className="logo">
-            <img src="https://seeklogo.com/images/S/skinstore-logo-5281C41BD3-seeklogo.com.png" alt="" />
-        </div>
-        <div className="search">search</div>
-        <div className="account">account</div>
-        <div className="cart">cart</div>
-    </div>
-  )
-}
+    <div className="nav">
+      <div className="logo">
+        <img
+          src="https://seeklogo.com/images/S/skinstore-logo-5281C41BD3-seeklogo.com.png"
+          alt=""
+        />
+      </div>
+      <div className="search">
+        <input
+          type="text"
+          name="search"
+          id=""
+          placeholder="Search for a product or brand"
+        />
+      </div>
+      <div className="account">
+        <p>
+          {" "}
+          <i class="fas">&#xf406;</i>Account
+        </p>
+      </div>
 
-export default Navbar
+      <div className="cart"><i class="fa-solid fa-cart-arrow-down"></i>Cart</div>
+
+      <div class="dropdown">
+                <button class="dropbtn">Makeup</button>
+                <div class="dropdown-content">
+                    <a href="../productpages/SkinCeuticals.html">View All Cousmetics</a>
+                    <a href="../productpages/SkinMedica.html">Eye Shadows</a>
+                    <a href="/manoj_kumar/Mascaras.html">Maskaras</a>
+                    <a href="/manoj_kumar/Bronzers.html">Bronzers </a>
+                    <a href="/manoj_kumar/Face Powders.html">Face Powders</a>
+                    <a href="/manoj_kumar/foundation.html">Foundations</a>
+                    <a href="/manoj_kumar/Lip_liners.html">Lips Liners</a>
+                    <a href="/manoj_kumar/Lipsticks.html">Lipsticks</a> 
+                    <a href="/manoj_kumar/Liquid_Lipstick.html">Liquid Lipsticks</a> 
+
+                </div>
+            </div>
+    </div>
+  );
+};
+
+export default Navbar;
