2c2f01c70ba9c405708de2fa7dbcb1660f6b7fc8
AnkurSinghYadav09 <ankursingh989@gmail.com>
2022-06-16 15:26:11 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './navbar/Navbar';
+import Carousal from './body/Carousal';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function App() {
   return (
     <div >
       <Navbar/>
+      <Carousal/>
     </div>
   );
 }
---
+++ src/body/Carousal.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+const Carousal = () => {
+  return (
+    <div>
+      <Carousel autoPlay infiniteLoop>
+        <div>
+          <img
+            alt=""
+            src="https://guileless-custard-2b667a.netlify.app/HOME-PAGE/slidingImage2.PNG"
+          />
+          <p className="legend">product 1</p>
+        </div>
+        <div>
+          <img
+            alt=""
+            src="https://guileless-custard-2b667a.netlify.app/HOME-PAGE/slidingImage1.PNG"
+          />
+          <p className="legend">product 2</p>
+        </div>
+        <div>
+          <img
+            alt=""
+            src="https://guileless-custard-2b667a.netlify.app/HOME-PAGE/slidingImage3.PNG"
+          />
+          <p className="legend">product 3</p>
+        </div>
+        <div>
+          <img
+            alt=""
+            src="https://static.thcdn.com/images/xlarge/webp/widgets/121-us/22/0519-STDCRE-36320-SS-LW-May-Beauty-Bag-Amend-1180x450-022122.jpg"
+          />
+          <p className="legend">product 4</p>
+        </div>
+        <div>
+          <img
+            alt=""
+            src="https://static.thcdn.com/images/xlarge/webp/widgets/121-us/42/original-0531-STDCRE-36879-AP-SS-JUNE-SUMMER-SHOT-03-1180x450-063342.jpg"
+          />
+          <p className="legend">product 5</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Carousal;
