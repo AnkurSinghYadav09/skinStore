e641c179d1cd18c01f604d94f4f3a6b10fb3c6cc
AnkurSinghYadav09 <ankursingh989@gmail.com>
2022-06-17 07:03:11 +0530

    update

--- src/components/Product1.jsx
+++ src/components/Product1.jsx
@@ -4,12 +4,14 @@ import "./Product1.css";
 const Product1 = (props) => {
   console.log(props.Data);
   return (
-    <div>
+    <div className="main">
       {props.Data.map((e) => (
         //   console.log(e)
         <div className="circle">
           <img src={e.productPic} alt="" />
-          <p>{}</p>
+          <p>{e.productName}</p>
+          <h5>${e.priceOfProduct}</h5>
+          <button>SHOP NOW</button>
         </div>
       ))}
     </div>
